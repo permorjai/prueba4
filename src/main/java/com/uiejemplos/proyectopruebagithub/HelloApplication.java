c991c0952d0bf6cd57b1f9c43f9704c617a39970
permorjai <jaime@cb.cbtis182.edu.mx>
2024-10-08 11:54:46 -0600

    Texto del boton cambió por segunda vez

--- src/main/java/com/uiejemplos/proyectopruebagithub/HelloApplication.java
+++ src/main/java/com/uiejemplos/proyectopruebagithub/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hola mundo 3°B, los mejores programadores!");
+        stage.setTitle("Hola mundo 3°B, los mejores programadores dek cbtis!");
         stage.setScene(scene);
         stage.show();
     }
